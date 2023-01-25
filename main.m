01d7af5e487aa2f3363a8844a579d3887db5556a
Giulio Nenna <giulio524@gmail.com>
2023-01-25 16:31:10 +0100

    iniziato punto 3 (da stampare le matrici di autovalori)

--- main.m
+++ main.m
@@ -2,7 +2,7 @@ clear
 close all
 clc
 
-print_fig = true; %Set TRUE for Printing figures to file
+print_fig = false; %Set TRUE for Printing figures to file
 %% ---- LOADING DATA -----
 load("data\Circle.mat", "X");
 circle_X = X;
@@ -13,14 +13,14 @@ clear X;
 fig(1) = figure;
 scatter(circle_X(:,1), circle_X(:,2), 10, 'filled');
 set(fig(1),'PaperSize',[14 11]);
-if print_fig
+if print_fig==true
     print(fig(1), 'Latex\pictures\circle_scatterplot.pdf', '-dpdf')
 end
 
 fig(1) = figure;
 scatter(spiral_X(:,1), spiral_X(:,2), 10, 'filled');
 set(fig(1),'PaperSize',[14 11]);
-if print_fig
+if print_fig == true
     print(fig(1), 'Latex\pictures\spiral_scatterplot.pdf', '-dpdf')
 end
 
@@ -41,21 +41,22 @@ for i=1:length(K)
     fig(2) = figure;
     plot(G_circle{i}, 'XData', circle_X(:,1), 'YData', circle_X(:,2))
     set(fig(2),'PaperSize',[14 11]);
-    if print_fig
+    if print_fig == true
     print(fig(2), ['Latex\pictures\circle_KNN_K' int2str(K(i)) '.pdf'], '-dpdf')
     end
 
     fig(2) = figure;
     plot(G_spiral{i}, 'XData', spiral_X(:,1), 'YData', spiral_X(:,2))
     set(fig(2),'PaperSize',[14 11]);
-    if print_fig
-    print(fig(2), ['Latex\pictures\spiral_KNN_K' int2str(K(i)) '.pdf'], '-dpdf')
+    if print_fig == true
+        print(fig(2), ['Latex\pictures\spiral_KNN_K' int2str(K(i)) '.pdf'], '-dpdf')
     end
 
 end
 
 
 %% --- TASK 2 ---
+
 for i=1:length(K)
     [L_circle{i}, D_circle{i}] = graph_laplacian(W_circle{i});
     [L_spiral{i}, D_spiral{i}] = graph_laplacian(W_spiral{i});
@@ -63,30 +64,41 @@ for i=1:length(K)
     fig(3) = figure;
     spy(L_circle{i})
     set(fig(3),'PaperSize',[14 11]);
-    if print_fig
-    print(fig(3), ['Latex\pictures\circle_LaplacianSpy_K' int2str(K(i)) '.pdf'], '-dpdf')
+    if print_fig == true
+        print(fig(3), ['Latex\pictures\circle_LaplacianSpy_K' int2str(K(i)) '.pdf'], '-dpdf')
     end
 
     fig(3) = figure;
     spy(L_spiral{i})
     set(fig(3),'PaperSize',[14 11]);
-    if print_fig
-    print(fig(3), ['Latex\pictures\spiral_LaplacianSpy_K' int2str(K(i)) '.pdf'], '-dpdf')
+    if print_fig == true
+        print(fig(3), ['Latex\pictures\spiral_LaplacianSpy_K' int2str(K(i)) '.pdf'], '-dpdf')
     end
 end
-%% --- TASK 3-4-5 ---
-M=3;
-[U_circle, eigs_circle] = eigs(L_circle, 6, 'smallestabs');
-%circle has clearly 2 connected components since the multiplicity of
-%eigenvalue 0 is 2. Eigenvalues that are "almost 0" are relative to "almost
-%connected components" Hence the number of cluster should be equal to the
-%number of eigenvalues that are "almost 0"
-[U_spiral, eigs_spiral] = eigs(L_spiral, 6, 'smallestabs');
+%% --- TASK 3 ---
+n_eigs=6;
+U_circle={};
+U_spiral={};
+eigs_circle={};
+eigs_spiral={};
+
+for i=1:length(K)
+    [U_circle{i}, eigs_circle{i}] = eigs(L_circle{i}, n_eigs, 'smallestabs');
+    [U_spiral{i}, eigs_spiral{i}] = eigs(L_spiral{i}, n_eigs, 'smallestabs');
+end
+
+for i = 1:length(K)
+    eigs_circle_tot(:,i) = diag(eigs_circle{i});
+    eigs_spiral_tot(:,i) = diag(eigs_spiral{i});
+end
+
+
+
 %Same for spiral but here we only have one connected component and 3
 %clusters.
 
-U_circle = U_circle(:,1:M);
-U_spiral = U_spiral(:,1:M);
+%U_circle = U_circle(:,1:M);
+%U_spiral = U_spiral(:,1:M);
 
 %% --- TASK 6-7---
 idx_circle = kmeans(U_circle, M);
